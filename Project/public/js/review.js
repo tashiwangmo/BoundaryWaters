5bf9b8235106420a7bb15fd9ca36a9402715b6e4
tashiwangmo <tsultrimlhewa@Tsultrims-MacBook-Air.local>
2018-09-08 22:04:36 -0500

    final

---
+++ Project/public/js/review.js
@@ -0,0 +1,58 @@
+console.log("review.js loaded");
+
+$(document).on("click", ".updateReview", editReview);
+$(document).on("keyup", ".inputBox", finishEdit);
+
+// This function dynamically changes the div into an input
+function editReview() {
+  var id = $(this).data('bwatersid');
+  var reviewDiv = $(`div[data-bwatersid = ${id}]`)
+  var currentReview = reviewDiv.text();
+  
+  console.log(currentReview);
+  var inputBox = $(`<input class="inputBox" type="text" data-bwatersid=${id}></input>`);
+  inputBox.val(currentReview);
+  reviewDiv.replaceWith(inputBox); 
+}
+// This function updates the comment portion of the review if a user hits the "Enter Key"
+function finishEdit(event) {
+  if (event.which === 13) {
+
+    var updatedReview = $(this).val().trim()
+    var id = $(this).data('bwatersid');
+    $(this).blur();
+    console.log(id);
+    console.log(updatedReview);
+    updateReview(id, updatedReview);
+  }
+}
+// This function updates comment in our database
+function updateReview(id, updatedReview) {
+  $.ajax("/api/bwaters/" + id, {
+    type: "PUT",
+    data: {
+    comments: updatedReview
+    }
+  }).then(function(DBValue,reviewDiv ,inputBox) {
+    console.log(DBValue);
+    var reviewDiv = $(`div[data-bwatersid = ${id}]`)
+    var inputBox = $(`<input class="inputBox" type="text" data-bwatersid=${id}></input>`);
+    inputBox.replaceWith(reviewDiv);
+    console.log(inputBox);
+    // location.reload();
+  });
+}
+
+// This function deletes the entry from the database and for the website.
+$(document).ready(function() { 
+  $(".deleteReview").click(function(event) {
+    event.preventDefault();
+    var id = $(this).data("bwatersid");
+    $.ajax("/api/bwaters/" + id, {
+      type: "DELETE"
+    }).then(function() {
+      console.log("Deleted review: ", id);
+      location.reload();
+    });
+  });
+});
---
+++ Project/public/js/submitInfo.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+  // Submit click function
+  $("#myForm").submit(function(event) {
+    event.preventDefault();
+
+    //Gather user input
+    var userInput = {
+      // Capture the user first name
+      firstName: $("#firstName")
+        .val()
+        .trim(),
+      //Capture the user last name
+      lastName: $("#lastName")
+        .val()
+        .trim(),
+      // Capture the user comments
+      comments: $("#comments")
+        .val()
+        .trim(),
+      // Capture the five question ratings
+      Q1: +$("#Q1")
+        .val()
+        .trim(),
+      Q2: +$("#Q2")
+        .val()
+        .trim(),
+      Q3: +$("#Q3")
+        .val()
+        .trim(),
+      Q4: +$("#Q4")
+        .val()
+        .trim(),
+      Q5: +$("#Q5")
+        .val()
+        .trim()
+    };
+    console.log(userInput);
+    $.post("/api/bwaters", userInput).done(function() {
+      console.log("Posted new data to database.");
+
+      $("#myModal").modal("toggle");
+      $("#myModal").on("shown.bs.modal", function() {
+        $("#myForm").trigger("focus");
+      });
+    });
+  });
+});
